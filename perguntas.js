d628c2224297c8ce9b1ab266f93f71506384cef8
mariastaudt <maria.staudt@escola.pr.gov.br>
2025-07-07 11:38:23 -0300

    sdf

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,17 @@
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    futebol,
+    Que ano o Grêmio foi fundado?,
+    1903
+)
+
+criaCartao(
+    vôlei,
+    Quantas medalhas de ouro das olimpíadas a seleção brasileira feminina de vôlei tem?,
+    2
+)
+
+criaCartao(
+    matemática,
+    Quanto é 608x46?,
+    27968
 )
