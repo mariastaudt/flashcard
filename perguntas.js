6cdd64ac0c1b7fc65e7c8ae92fc75f9ddcdb8490
mariastaudt <maria.staudt@escola.pr.gov.br>
2025-08-11 08:46:55 -0300

    asdf

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,35 @@
 criaCartao(
-    futebol,
-    Que ano o Grêmio foi fundado?,
-    1903
+    'futebol',
+    'Que ano o Grêmio foi fundado?',
+    '1903'
 )
 
 criaCartao(
-    vôlei,
-    Quantas medalhas de ouro das olimpíadas a seleção brasileira feminina de vôlei tem?,
-    2
+    'vôlei',
+    'Quantas medalhas de ouro das olimpíadas a seleção brasileira feminina de vôlei tem?',
+    '2'
 )
 
 criaCartao(
-    matemática,
-    Quanto é 608x46?,
-    27968
+    'matemática',
+    'Quanto é 608x46?',
+    '27968'
+)
+
+criaCartao(
+    'história',
+    'Que ano o Titanic afundou?',
+    '1912'
+)
+
+criaCartao(
+    'futebol',
+    'Que ano o Flamengo foi fundado?',
+    '1895'
+)
+
+criaCartao(
+    'cinema',
+    'Qual é o vingador que usa armadura de ferro?',
+    'Homem de Ferro'
 )
